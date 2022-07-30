57954a7e2cbea14b62fc9ff877f4b1d810b60d3e
Ferdiansyah <ferdikings03@gmail.com>
2022-07-30 23:49:07 +0700

    HITUNG_DICODING

---
+++ contoh variable.js
@@ -0,0 +1,18 @@
+// comtoh var 
+
+var firstName = "Harry";
+console.log(firstName);
+
+firstName = "Ron";
+console.log(firstName);
+
+/* output: 
+Harry
+Ron
+*/
+
+// contoh hoisting 
+
+x = 100;
+var x;
+console.log(x);
